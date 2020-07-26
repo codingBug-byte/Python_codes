028c89dddc273b7701da83b814515058875e1827
codingBug-byte <31822279+codingBug-byte@users.noreply.github.com>
2020-07-26 17:45:56 +0530

    Update capitalize.py

--- Hacker_rank_python_skills/capitalize.py
+++ Hacker_rank_python_skills/capitalize.py
@@ -5,9 +5,4 @@ def solve(s):
         s = s.replace(x, x.capitalize())
     return s
 
-    # first, last = s.split()
-    # first =first[0].upper() + first[1:]
-    # last =last[0].upper() + last[1:]
-    # s = first +" "+ last
-    # print(s)
-
+   
