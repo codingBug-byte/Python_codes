0295e471e05de61f214717ac5dd79e1397de90c1
codingBug-byte <31822279+codingBug-byte@users.noreply.github.com>
2020-07-27 13:18:31 +0530

    Update door_mat.py

--- Hacker_rank_python_skills/door_mat.py
+++ Hacker_rank_python_skills/door_mat.py
@@ -18,14 +18,3 @@ for i in reversed(range(1,n,2)):
 
 
 
-#
-# n = int(input())
-# dec = []
-# for i in range(1,int(n**0.5)):
-#     if n%i == 0:
-#         print(i,end = " ")
-#         if not ( n / i == i):
-#             dec.append(int( n / i))
-# dec.sort()
-# for i in dec:
-#     print(i,end = " ")
