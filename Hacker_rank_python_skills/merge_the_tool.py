08c1d80f2ec6c03a3749fb9dc8314a0e7cb059ab
codingBug-byte <31822279+codingBug-byte@users.noreply.github.com>
2020-07-27 13:19:52 +0530

    Update merge_the_tool.py

--- Hacker_rank_python_skills/merge_the_tool.py
+++ Hacker_rank_python_skills/merge_the_tool.py
@@ -1,21 +1,6 @@
-
-# from collections import OrderedDict
-# def merge_the_tools(string, k):
-#     substrings = [ string[i:i+k] for i in range(0,len(string),k)]
-#     for i in range(len(substrings)):
-#         substrings[i] = "".join(OrderedDict.fromkeys(substrings[i]))
-#     for i in substrings:
-#         print(i)
-
-
 S, N = input(), int(input())
 print(list(iter(S)))
 print(list(zip(*[iter(S)] * N)))
 for part in zip(*[iter(S)] * N):
     d = dict()
     print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))
-
-
-# if __name__ == '__main__':
-#     string, k = input(), int(input())
-#     merge_the_tools(string, k)
